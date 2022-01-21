175c0cc4ea108e42441d8e9e94b97518f6633a3b
Рафиков Айдар Робертович <190210@edu.psuti.ru>
2022-01-21 18:26:49 +0400

    silent commit

--- main.py
+++ main.py
@@ -21,13 +21,18 @@ def t_updater():
             lock.acquire()
             lut = last_update_time
             lock.release()
-            if time.time() - lut > 2 * sender_period:
+            time_passed = time.time() - lut
+            if time_passed > get_allowed_skips() * sender_period:
                 internet_status_dead()
             else:
-                internet_status_alive()
+                internet_status_alive(time_passed)
         except Exception as e:
             logging.info(e)
             send_status("Поток умер изза эксепшена (см логи)")
+
+
+def get_allowed_skips():
+    return ALLOWED_SKIPS + 2  # VERY DIFFICULT LOGIC, JUST TRUST ME
 
 
 def get_check_period() -> float:
@@ -43,22 +48,30 @@ def internet_status_dead():
         return
 
     _is_internet_alive = False
-    send_status(STATUS_DEAD)
+    send_silent_status(STATUS_DEAD)
 
 
-def internet_status_alive():
+def internet_status_alive(time_passed):
     global _is_internet_alive
     if _is_internet_alive:  # already alive - skip
         return
 
     _is_internet_alive = True
-    send_status(STATUS_ALIVE)
+    status = STATUS_ALIVE.format(time_passed)
+    if time_passed < SILENT_TIME_MINUTES*60:
+        send_status(status)
+    else:
+        send_silent_status(status)
 
 
-def send_status(status: str):  # todo subscribing
-    r = vk.rest.post("messages.send", user_id=user_id, message=status, random=True)
+def send_status(status: str, silent=0):  # todo subscribing (upd 21.01.2022: no, thanks :) )
+    r = vk.rest.post("messages.send", user_id=user_id, message=status, random=True, silent=silent)
     logging.info(status)
     logging.info(r.json())
+
+
+def send_silent_status(status: str):
+    send_status(status, silent=1)
 
 
 def no_slash(bytes):
@@ -90,7 +103,10 @@ sender_period = 60
 alive_check_period = sender_period / 2
 dead_check_period = sender_period / 4
 
-STATUS_ALIVE = "Интернет ожил во имя святого Лемжина"
+ALLOWED_SKIPS = 2
+SILENT_TIME_MINUTES = 5
+
+STATUS_ALIVE = "Интернет ожил во имя святого Лемжина (спустя {0} секунд)"
 STATUS_DEAD = "Инет упал, опять.."
 
 if __name__ == '__main__':
